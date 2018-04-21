8a004ed178ac77d7b337537ed98d7101e623f300
Thiago Loureiro <thiagoguaru@outlook.com>
2018-04-21 13:25:48 +0200

    Added Test Project

---
+++ Dapper.Crud.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Dapper.Crud.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Dapper.Crud.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b360c8ad-69e6-4046-bec6-e240dd5c6c51")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Dapper.Crud.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Dapper.Crud.VSExtension.Helpers;
+using Xunit;
+
+namespace Dapper.Crud.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void GenerateSelect()
+        {
+            var ret = DapperGenerator.Select("ModelTest");
+            Assert.True(ret.Contains("SELECT * FROM [ModelTest]"));
+            Assert.True(ret.Contains("ret = db.Query<ModelTest>(sql, commandType: CommandType.Text).ToList();"));
+        }
+    }
+}
