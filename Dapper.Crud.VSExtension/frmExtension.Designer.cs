0cf1d6c360c8633061af009127c035fb115fe516
Thiago Loureiro <thiagoguaru@me.com>
2018-08-13 00:39:29 -0300

    Changed Version Number

--- Dapper.Crud.VSExtension/frmExtension.Designer.cs
+++ Dapper.Crud.VSExtension/frmExtension.Designer.cs
@@ -312,7 +312,7 @@
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmExtension";
-            this.Text = "Dapper Extension";
+            this.Text = "Dapper Extension 2.1";
             this.gBox.ResumeLayout(false);
             this.gBox.PerformLayout();
             this.groupBox1.ResumeLayout(false);
