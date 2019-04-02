84e8cbcd6034e63056051deb102bbbf817d6baa4
Thiago Loureiro <thiagoguaru@me.com>
2019-04-03 00:22:11 +0200

    VS 2009 Support

--- Dapper.Crud.VSExtension/CreateCrudPackage.cs
+++ Dapper.Crud.VSExtension/CreateCrudPackage.cs
@@ -4,15 +4,17 @@ using Microsoft.VisualStudio.Shell;
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
+using System.Threading;
+using Task = System.Threading.Tasks.Task;
 
 namespace Dapper.Crud.VSExtension
 {
-    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [Guid(CreateCrudPackage.PackageGuidString)]
     [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
-    public sealed class CreateCrudPackage : Package
+    public sealed class CreateCrudPackage : AsyncPackage
     {
         public DTE2 Dte;
 
@@ -23,24 +25,16 @@ namespace Dapper.Crud.VSExtension
         /// </summary>
         public const string PackageGuidString = "cb7802c6-dd51-4368-ad9a-413841cf0287";
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="CreateCrud"/> class.
-        /// </summary>
-        public CreateCrudPackage()
-        {
-        }
-
         #region Package Members
 
-        /// <summary>
-        /// Initialization of the package; this method is called right after the package is sited, so this is the place
-        /// where you can put all the initialization code that rely on services provided by VisualStudio.
-        /// </summary>
-        protected override void Initialize()
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
+            // When initialized asynchronously, the current thread may be a background thread at this point.
+            // Do any initialization that requires the UI thread after switching to the UI thread.
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
             CreateCrud.Initialize(this);
-            base.Initialize();
-            Dte = GetService(typeof(DTE)) as DTE2;
+            await base.InitializeAsync(cancellationToken, progress);
+            Dte = await GetServiceAsync(typeof(DTE)) as DTE2;
             Instance = this;
             Logger.Initialize(this, "Dapper CRUD Generator");
         }
